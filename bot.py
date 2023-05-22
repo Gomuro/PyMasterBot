8e2ac0a343fe3cc1e355f0cd5af121de5ccf6734
Borschevskyi <Borsch2556@gmail.com>
2023-05-22 23:10:27 +0300

    Edited and moved functions into separate file

--- bot.py
+++ bot.py
@@ -1,176 +1,128 @@
-"""import necessary libraries"""
-import subprocess
-import re
-import logging
-import os
-import json
-import datetime
+"""Imports"""
 import telebot
 
+from utils.Handlers.callback_query_handler import callback_query_handler
+from utils.Handlers.check_code_handler import check_code
+from utils.Handlers.documentation_handler import search_documentation
+from utils.Handlers.help_handler import help_handler
 from utils.KeyBoard.key_board import Keyboard
-
-# set the file path for the token
-FILE_NAME = 'data/bot_token.txt'
-
-# read the token from the file
-with open(FILE_NAME, 'r', encoding = 'utf-8') as token_file:
-    TOKEN = token_file.read().strip()
-
-# create a telebot instance using the token
-bot = telebot.TeleBot(TOKEN)
-inline_keyboard = Keyboard()
-
-TMP_FILE = 'tmp.py'
-RELATIVE_PATH = 'data/logs'
-ABSOLUTE_PATH = os.path.abspath(RELATIVE_PATH)
-LOG_FILE_FORMAT = '%Y-%m-%d.json'
+from utils.Modes import MODE_DOCUMENTATION, MODE_MAIN_MENU, MODE_CHECK_CODE
 
 
-def setup_logging():
-    """Logger setup"""
-    os.makedirs(ABSOLUTE_PATH, exist_ok = True)
-
-    logger = logging.getLogger('PyMasterBot')
-    logger.setLevel(logging.INFO)
-
-    current_date = datetime.datetime.now().strftime(LOG_FILE_FORMAT)
-    log_file = os.path.join(ABSOLUTE_PATH, current_date)
-    file_handler = logging.FileHandler(log_file)
-
-    logger.addHandler(file_handler)
-
-    return logger
-
-
-def log_message(command, code, text):
+class Bot:
     """
-    Logs the message details to a JSON log file.
-
-    Args:
-        command (str): The command that triggered the logging.
-        code (str): The code associated with the log message.
-        text (str): The text message to be logged.
-
-    Returns:
-        None
+    This class implements the bot
     """
-    timestamp = datetime.datetime.now()
 
-    log_data = {
-        'timestamp': timestamp.strftime('%Y-%m-%d %H:%M:%S'),
-        'command': command,
-        'code': code,
-        'response': text,
-    }
+    def __init__(self, token_file):
+        self.inline_keyboard = None
+        self.bot = None
+        self.token_file = token_file
+        self.current_mode = MODE_MAIN_MENU
 
-    log_file = os.path.join(ABSOLUTE_PATH, datetime.datetime.now().strftime(LOG_FILE_FORMAT))
+    def run(self):
+        """
+        This method runs the bot
+        :return:
+        """
+        # read the token from the file
+        with open(self.token_file, 'r', encoding='utf-8') as file:
+            token = file.read().strip()
 
-    if not os.path.exists(log_file):
-        with open(log_file, 'w', encoding = 'utf-8') as file:
-            json.dump({'data': [log_data]}, file, ensure_ascii = False, indent = 4)
-    else:
-        with open(log_file, 'r', encoding = 'utf-8') as file:
-            try:
-                existing_data = json.load(file)
-            except json.JSONDecodeError:
-                existing_data = {}
+        # create a telebot instance using the token
+        self.bot = telebot.TeleBot(token)
 
-        existing_data.setdefault('data', []).append(log_data)
+        # create an instance of InlineKeyboardMarkup
+        self.inline_keyboard = Keyboard()
 
-        with open(log_file, 'w', encoding = 'utf-8') as file:
-            json.dump(existing_data, file, ensure_ascii = False, indent = 4)
+    def get_bot(self):
+        """
+        This method returns the bot
+        """
+        return self.bot
+
+    def set_current_mode(self, mode):
+        """
+        This method sets the current mode
+        :param mode:
+        :return:
+        """
+        self.current_mode = mode
 
 
-# handle the /start command
-@bot.message_handler(commands=['start'])
-def start(message):
+bot = Bot('data/bot_token.txt')
+bot.run()
+
+telebot_instance = bot.get_bot()
+
+
+@telebot_instance.message_handler(commands=['start'])
+def start_handler(message):
     """
-    Send a welcome message to the user with a keyboard for further interaction.
-    Args:
-        message (telebot.types.Message): The message object that triggered the function.
-    Returns:
-        None
+    This method sends the welcome message
+    :param message:
+    :return:
     """
-    text = 'Привіт! Я бот, який може перевірити ваш код.\n' \
-           'Напишіть одним повідомленням /check_code - ваш код.'
-    log_message('/start', "", text,)
-    bot.send_message(message.chat.id,
-                     text=text,
-                     reply_markup=inline_keyboard.get_keyboard())
+    telebot_instance.send_message(message.chat.id, "Welcome to my bot!",
+                                  reply_markup=bot.inline_keyboard.get_keyboard())
 
 
-@bot.message_handler(commands = ['check_code'])
-def check_code(message):
+@telebot_instance.message_handler(commands=['check_code'])
+def check_syntax_handler(message):
+    """This handler allows to send designated message when the button been pressed"""
+    bot.current_mode = MODE_CHECK_CODE
+    check_code(message, telebot_instance)
+
+
+@telebot_instance.message_handler(commands=['help'])
+def handle_help(message):
+    """This handler allows to send designated message when the button been pressed"""
+    help_handler(message, telebot_instance, bot.inline_keyboard)
+
+
+@telebot_instance.message_handler(commands=['documentation'])
+def documentation_handler(message):
     """
-    :param message: create a temporary file and write the code into it
-    :return: send message to user
+    use the search_documentation function
+    :param message:
+    :return:
     """
-    try:
-        code = message.text.split(maxsplit = 1)[1].strip()
-    except IndexError:
-        text = 'Будь ласка, вкажіть код для перевірки.'
-        log_message('/check_code', "", text,)
-        bot.send_message(message.chat.id, text = text)
-        return
-
-    # Create a temporary file and write the code into it
-    with open(TMP_FILE, 'w', encoding = 'utf-8') as checked_file:
-        checked_file.write(code + "\n")
-
-    # Check PEP8 style
-    pep8_output = check_style()
-
-    # Check syntax
-    syntax_errors = check_syntax()
-
-    result = ""
-
-    if syntax_errors:
-        result += f"У вашому коді є синтаксична помилка:\n{syntax_errors}\n"
-    else:
-        result += "Код не має синтаксичних помилок.\n"
-
-    if pep8_output:
-        cleaned_output = re.sub(r'\*.*?\*\* Module tmp\ntmp.py:\d+:\d+: ', '', pep8_output)
-        result += f"\nУ вашому коді помилка PEP-8:\n{cleaned_output}"
-    else:
-        result += "\nКод не має помилок PEP-8."
-
-    log_message('/check_code', code, result)
-
-    bot.send_message(message.chat.id, result)
+    bot.current_mode = MODE_DOCUMENTATION
+    search_documentation(message, telebot_instance)
 
 
-def check_style():
-    """Compare this snippet from data/bot_token.txt:"""
-    result = subprocess.run(['pylint', TMP_FILE],
-                            stdout = subprocess.PIPE,
-                            stderr = subprocess.PIPE,
-                            check = False)  # check=False to not raise an exception
-    output = result.stdout.decode('utf-8') + result.stderr.decode('utf-8')
-    cleaned_output = output.replace(
-        '------------------------------------------------------------------', '')
+@telebot_instance.callback_query_handler(func=lambda call: True)
+def handle_callback_query(call):
+    """This handler allows to use callback query with pressing designated inline keyboard buttons"""
+    if call.data.find(MODE_DOCUMENTATION) != -1:
+        bot.current_mode = MODE_DOCUMENTATION
+    elif call.data.find(MODE_CHECK_CODE) != -1:
+        bot.current_mode = MODE_CHECK_CODE
+    elif call.data.find(MODE_MAIN_MENU) != -1:
+        bot.current_mode = MODE_MAIN_MENU
+    callback_query_handler(call, telebot_instance, bot.inline_keyboard)
 
-    return cleaned_output.strip() if cleaned_output else None
+
+# Додано новий обробник повідомлень для відображення поточного режиму
+@telebot_instance.message_handler(func=lambda message: True)
+def display_current_mode(message):
+    """
+    This method displays the current mode
+    :param message:
+    :return:
+    """
+    if bot.current_mode == MODE_MAIN_MENU:
+        telebot_instance.send_message(message.chat.id, "Знаходитесь в головному меню.")
+    elif bot.current_mode == MODE_DOCUMENTATION:
+        telebot_instance.send_message(message.chat.id, "Знаходитесь в режимі документації.")
+        search_documentation(message, telebot_instance)
+    elif bot.current_mode == MODE_CHECK_CODE:
+        telebot_instance.send_message(message.chat.id, "Знаходитесь в режимі перевірки коду.")
+        check_code(message, telebot_instance)
 
 
-def check_syntax():
-    """Compare this snippet from tmp.py:"""
-    # create a subprocess to run python command with the file
-    with subprocess.Popen(['python', TMP_FILE], stdin = subprocess.PIPE,
-                          stdout = subprocess.PIPE,
-                          stderr = subprocess.PIPE,
-                          shell = False) as process:
-        # write the code to the subprocess stdin and close it
-        error = process.communicate()
-    # if there is an error, send it as a message to the user
-    if error[1]:
-        # remove the unnecessary from the error message
-        error = error[1].decode().split('\n')[-2]
-        return error
-    return None
-
+telebot_instance.callback_query_handler(func=lambda call: True)(callback_query_handler)
 
 # start polling for new messages
 if __name__ == '__main__':
-    bot.polling()
+    telebot_instance.polling(none_stop=True, timeout=60)
