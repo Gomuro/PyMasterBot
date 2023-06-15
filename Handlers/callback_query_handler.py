9de085c416e02a9a74699618272909fa51a425cf
Borschevskyi <Borsch2556@gmail.com>
2023-06-15 16:04:59 +0300

    restructuring

--- Handlers/callback_query_handler.py
+++ Handlers/callback_query_handler.py
@@ -6,32 +6,17 @@ def callback_query_handler(call, telebot_instance, inline_keyboard):
 
     telebot_instance.delete_message(call.message.chat.id, call.message.message_id)
 
-    if call.data == "/help":
+    if call.data == '/check_code':
         telebot_instance.answer_callback_query(callback_query_id=call.id)
-        telebot_instance.send_message(
-            chat_id=call.message.chat.id,
-            text="I can help you with these commands:\n"
-            "/help - help\n"
-            "/check_code - syntax check\n"
-            "/documentation - documentation",
-            reply_markup=inline_keyboard.get_keyboard(),
-        )
-    elif call.data == "/check_code":
-        telebot_instance.answer_callback_query(callback_query_id=call.id)
-        telebot_instance.send_message(
-            chat_id=call.message.chat.id,
-            text="Send the code you want to check.",
-            reply_markup=inline_keyboard.get_keyboard(),
-        )
+        telebot_instance.send_message(chat_id=call.message.chat.id,
+                                      text="Send the code you want to check.",
+                                      reply_markup=inline_keyboard.get_keyboard())
 
-    elif call.data == "/documentation":
+    elif call.data == '/documentation':
         telebot_instance.answer_callback_query(callback_query_id=call.id)
-        telebot_instance.send_message(
-            chat_id=call.message.chat.id,
-            text="Enter the name of the module, function, or class: ",
-            reply_markup=inline_keyboard.get_keyboard(),
-        )
+        telebot_instance.send_message(chat_id=call.message.chat.id,
+                                      text="Enter the name of the module, function, or class: ",
+                                      reply_markup=inline_keyboard.get_keyboard())
     else:
-        telebot_instance.answer_callback_query(
-            callback_query_id=call.id, text="This command does not exist!"
-        )
+        telebot_instance.answer_callback_query(callback_query_id=call.id,
+                                               text="This command does not exist!")
