fa7c8f5c2aa8a715f1fc2f89f8f80d4ab8dfba4f
kukalets-sergiy <kukaletssergiy@gmail.com>
2023-06-28 09:30:01 +0300

    added functionality so that questions are not repeated

--- Handlers/choose_test_task_handler.py
+++ Handlers/choose_test_task_handler.py
@@ -11,8 +11,14 @@ def choose_test_task_function(message, bot):
         # Якщо не знайдено тестових завдань за обраним рівнем, відправляємо повідомлення про помилку
         bot.reply_to(message, "Не знайдено тестових завдань за обраним рівнем.")
     else:
-        # Вибір випадкового тестового завдання
-        test_task = choice(test_tasks)
+        used_questions = []  # Список для збереження використаних питань
+        if len(used_questions) == len(test_tasks):
+            used_questions = []  # Якщо всі питання вже були використані, очищуємо список
+
+        # Вибір випадкового тестового завдання, яке ще не було використане
+        test_task = choice([task for task in test_tasks if task[2] not in used_questions])
+        used_questions.append(test_task[2])  # Додаємо питання до списку використаних
+
         question = test_task[2]
         var1 = test_task[3]
         var2 = test_task[4]
@@ -21,3 +27,4 @@ def choose_test_task_function(message, bot):
 
         response = f"{question}\n\n1. {var1}\n2. {var2}\n3. {var3}"
         bot.reply_to(message, response)
+
