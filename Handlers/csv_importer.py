458549b0a327fa82a61b719e5ef88678b9377fb4
kukalets-sergiy <kukaletssergiy@gmail.com>
2023-07-26 19:29:41 +0300

    added CodeTask table and functionality to add code_tasks by adding a csv file

--- Handlers/csv_importer.py
+++ Handlers/csv_importer.py
@@ -127,6 +127,72 @@ def add_test_tasks_csv(session, csv_filename, TestTask):
                 session.commit()
 
 
+def add_code_tasks_csv(session, csv_filename, CodeTask):
+    pass
+    with open(csv_filename, 'r') as file:
+        reader = csv.reader(file)
+        next(reader)  # Skip the first row (headers)
+
+        for row in reader:
+            if not row:  # Skip empty rows
+                continue
+
+            number = row[0]
+            if not number.isdigit():
+                continue  # Skip rows without a numerical number
+
+            topic = row[1]
+            question = row[2]
+            var1 = row[3]
+            var2 = row[4]
+            var3 = row[5]
+            right_answer = row[6]
+            level_relation = row[7]
+
+            # Check if the row already exists in the database
+            existing_row = session.query(CodeTask).filter_by(
+                topic=topic,
+                question=question,
+                var1=var1,
+                var2=var2,
+                var3=var3,
+                right_answer=right_answer,
+                level_relation=level_relation
+            ).first()
+
+            if existing_row:
+                continue  # Skip adding the row if it already exists
+
+            # Get the maximum number from the database
+            last_number = session.query(func.max(CodeTask.id)).scalar() or 0
+
+            try:
+                # Create a new task in the database
+                new_lesson = CodeTask(
+                    id=last_number + 1,  # Set the new number as the maximum + 1
+                    topic=topic,
+                    question=question,
+                    var1=var1,
+                    var2=var2,
+                    var3=var3,
+                    right_answer=right_answer,
+                    level_relation=level_relation
+                )
+                session.add(new_lesson)
+                session.commit()
+            except sqlalchemy.exc.IntegrityError:
+                # Handle unique constraint violation
+                session.rollback()
+                existing_lesson = session.query(CodeTask).filter_by(id=number).first()
+                existing_lesson.topic = topic
+                existing_lesson.question = question
+                existing_lesson.var1 = var1
+                existing_lesson.var2 = var2
+                existing_lesson.var3 = var3
+                existing_lesson.right_answer = right_answer
+                existing_lesson.level_relation = level_relation
+                session.commit()
+
 
 
 
